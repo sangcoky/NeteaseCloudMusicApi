371db08c288baedcd0aa0edca7a65e8e245a7400
sangcoky <106204700+sangcoky@users.noreply.github.com>
2022-05-25 10:55:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,3 @@
 require('./server').serveNcmApi({
   checkVersion: true,
 })
-var bodyParser = require('body-parser')
-app.use(bodyParser.json({limit:'50mb'}));
-app.use(bodyParser.urlencoded({limit:'50mb',extended:true}));
